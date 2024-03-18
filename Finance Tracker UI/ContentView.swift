b52456f6ec5dea8a0c42e1e53b65a83c8f3392da
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2024-03-18 21:34:10 +0530

    Initial Commit

---
+++ Finance Tracker UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance Tracker UI
+//
+//  Created by Prashant Nigam on 18/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance Tracker UI/Finance_Tracker_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_Tracker_UIApp.swift
+//  Finance Tracker UI
+//
+//  Created by Prashant Nigam on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Finance_Tracker_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
